47d7355fd3591aea9d3d7a5a686ccb656fad6f6d
Aktarul <aktar@mysoftltd.com>
2021-08-04 12:07:56 +0600

    design change

--- lib/message.dart
+++ lib/message.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:save_me_2/Helper.dart';
 import 'package:save_me_2/MyMapController.dart';
 import 'package:save_me_2/details_page.dart';
 
@@ -22,6 +23,7 @@ class _FirebaseMessagingDemoState extends State<FirebaseMessagingDemo> {
   Letlng letlng;
   var lat;
   var lng;
+  var titles = "a";
   _getToken() {
     _firebaseMessaging.getToken().then((deviceToken) {
       print("Device Token: $deviceToken");
@@ -35,7 +37,6 @@ class _FirebaseMessagingDemoState extends State<FirebaseMessagingDemo> {
     _messages = List<Message>();
     _getToken();
     _configureFirebaseListeners();
-
   }
 
   _configureFirebaseListeners() {
@@ -48,6 +49,7 @@ class _FirebaseMessagingDemoState extends State<FirebaseMessagingDemo> {
         print('onMessage: $message');
         // print('parsedMessage: $message');
         sender = message['notification']['title'];
+        titles = message['notification']['title'];
         parsedMessage = message['notification']['body'];
         // print('parsedMessage: $message');
         lat = message['data']['Lat'];
@@ -55,7 +57,7 @@ class _FirebaseMessagingDemoState extends State<FirebaseMessagingDemo> {
         lng = message['data']['Lng'];
         // lng = double.parse(lng);
         mymapcont.pointLat.value = double.tryParse(lat);
-        mymapcont.pointLong.value= double.tryParse(lng);
+        mymapcont.pointLong.value = double.tryParse(lng);
         print('lat: ${mymapcont.pointLat.value}');
         print('lng: ${mymapcont.pointLong.value}');
         print('parsedMessage: $parsedMessage');
@@ -97,13 +99,16 @@ class _FirebaseMessagingDemoState extends State<FirebaseMessagingDemo> {
     log('mydata: $title');
 
     if (title != null) {
-      var route = new MaterialPageRoute(
-          builder: (BuildContext context) => DetailsPage(
-                titless: title,
-                lat: this.lat,
-                lng: this.lng,
-              ));
-      Navigator.of(context).push(route);
+      // var route = new MaterialPageRoute(
+      //     builder: (BuildContext context) => DetailsPage(
+      //           titless: title,
+      //           lat: this.lat,
+      //           lng: this.lng,
+      //         ));
+      // Navigator.of(context).push(route);
+      setState(() {
+        this.titles = title;
+      });
     }
 
     setState(() {
@@ -119,7 +124,9 @@ class _FirebaseMessagingDemoState extends State<FirebaseMessagingDemo> {
           title: Text(widget.title),
         ),
         body: Center(
-          child: Text('location'),
+          child: this.titles != "a"
+              ? notificationCard(this.lat, this.lng, this.titles)
+              : Text('location'),
         )
 
         // ListView.builder(
@@ -140,6 +147,79 @@ class _FirebaseMessagingDemoState extends State<FirebaseMessagingDemo> {
         //   },
         // ),
         );
+  }
+
+  Widget notificationCard(var l, var d, var t) {
+    return Container(
+      height: 66,
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.only(top: 5, bottom: 5, left: 20, right: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: InkWell(
+        onTap: () {
+          print("object");
+
+          // var a = t;
+          // print(a);
+          // var b = l;
+          // var c = d;
+          mymapcont.gotoMap();
+          // controller.setAddress(a, b, c);
+        },
+        child: Stack(
+          children: [
+            Positioned(
+              top: 16,
+              left: 10,
+              child: Container(
+                height: 35,
+                width: 35,
+                child: CircleAvatar(
+                  backgroundColor: Color(Helper.getHexToInt("#CDFFEF")),
+                  radius: 60.0,
+                  child: Container(
+                      child: Icon(
+                    Icons.location_on,
+                    size: 18,
+                    color: Color(Helper.getHexToInt("#11C4A1")),
+                  )),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 15,
+              left: 65,
+              child: Center(
+                child: Container(
+                  child: Text(
+                    t,
+                    style: TextStyle(
+                        fontFamily: "TTCommonsd",
+                        fontSize: 16,
+                        color: Color(Helper.getHexToInt("#000000"))),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+                top: 38,
+                left: 65,
+                child: Center(
+                  child: Text(
+                   "latitude: "+ l + ", " +"Longitude: "+ d,
+                    style: TextStyle(
+                        fontFamily: "TTCommonsd",
+                        fontSize: 14,
+                        color: Color(Helper.getHexToInt("#9F9F9F"))),
+                  ),
+                )),
+          ],
+        ),
+      ),
+    );
   }
 }
 
